b603a2cb6a4f99760ac6e25ea4919840749e9806
Stanley Espinoza <stanleysmart12@gmail.com>
2023-04-08 00:11:58 -0600

    Adding Firestore

---
+++ SalesMicroservice/Interfaces/IBaseFirestoreData.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesMicroservice.Interfaces
+{
+    public interface IBaseFirestoreData
+    {
+        /// <summary>
+        /// Gets and set the Id.
+        /// </summary>
+        public string Id { get; set; }
+    }
+}
--- SalesMicroservice/Models/SalesReport.cs
+++ SalesMicroservice/Models/SalesReport.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using Google.Cloud.Firestore;
+using SalesMicroservice.Interfaces;
+using System.Collections.Generic;
 
 namespace SalesMicroservice.Models
 {
-    public class SalesReport
+    [FirestoreData]
+    public class SalesReport : IBaseFirestoreData
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
+        public int Correlative { get; set; }
         public int CustomerId { get; set; }
         public Customer Customer { get; set; }
         public string Description { get; set; }
