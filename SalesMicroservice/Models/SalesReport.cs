585a25928f98596d6bb53faf5ec76938297bce33
Stanley Espinoza <stanleysmart12@gmail.com>
2023-04-07 22:54:20 -0600

    First Commit

---
+++ SalesMicroservice/Models/SalesReport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SalesMicroservice.Models
+{
+    public class SalesReport
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public string Description { get; set; }
+        public List<SalesReportDetail> Details { get; set; }
+    }
+}
